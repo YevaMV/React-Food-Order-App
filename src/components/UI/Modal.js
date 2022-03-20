bfc861ed7485e7841ba6f71a71ad250c05c253ad
YevaMV <eva280988@gmail.com>
2022-03-20 17:18:23 +0400

    managed Cart & Modal state

--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -3,7 +3,7 @@ import ReactDom from 'react-dom';
 import classes from './Modal.module.css';
 
 function Backdrop(props) {
-  return <div className={classes.Backdrop} />;
+  return <div className={classes.backdrop} onClick={props.onClose} />;
 }
 function ModalOverlay(props) {
   return (
@@ -14,10 +14,14 @@ function ModalOverlay(props) {
 }
 
 const portalElement = document.getElementById('overlays');
+
 function Modal(props) {
   return (
     <Fragment>
-      {ReactDom.createPortal(<Backdrop />, portalElement)}
+      {ReactDom.createPortal(
+        <Backdrop onClose={props.onClose} />,
+        portalElement
+      )}
       {ReactDom.createPortal(
         <ModalOverlay>{props.children}</ModalOverlay>,
         portalElement
