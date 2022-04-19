736fa46209289e95abd66afcd98b14f0c0ebd526
YevaMV <eva280988@gmail.com>
2022-04-19 17:01:17 +0400

    Working on a More Complex Reducer Logic, Making Items Removable

--- src/components/Card/Cart.js
+++ src/components/Card/Cart.js
@@ -11,8 +11,12 @@ function Cart(props) {
   const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
   const hasItems = cartCtx.items.length > 0;
 
-  const cartItemRemoveHandker = (id) => {};
-  const cartItemAddHandler = (item) => {};
+  const cartItemRemoveHandker = (id) => {
+    cartCtx.removeItem(id);
+  };
+  const cartItemAddHandler = (item) => {
+    cartCtx.addItem({ ...item, amount: 1 });
+  };
 
   const cartItems = (
     <ul className={classes['cart-items']}>
