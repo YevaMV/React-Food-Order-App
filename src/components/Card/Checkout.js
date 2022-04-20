7ae4b9c70741bc13c731d32ff1192718a9200811
YevaMV <eva280988@gmail.com>
2022-04-20 22:24:31 +0400

    added CSS and updated

--- src/components/Card/Checkout.js
+++ src/components/Card/Checkout.js
@@ -2,10 +2,11 @@ import classes from './Checkout.module.css';
 
 const Checkout = (props) => {
   const confirmHandler = (event) => {
-    event.prevetDefault();
+    event.preventDefault();
   };
+
   return (
-    <form onSubmit={confirmHandler}>
+    <form className={classes.form} onSubmit={confirmHandler}>
       <div className={classes.control}>
         <label htmlFor="name">Your Name</label>
         <input type="text" id="name" />
@@ -22,10 +23,12 @@ const Checkout = (props) => {
         <label htmlFor="city">City</label>
         <input type="text" id="city" />
       </div>
-      <button type="button" onClick={props.onCancel}>
-        Confirm
-      </button>
-      <button>Cancel</button>
+      <div className={classes.actions}>
+        <button type="button" onClick={props.onCancel}>
+          Cancel
+        </button>
+        <button className={classes.submit}>Confirm</button>
+      </div>
     </form>
   );
 };
